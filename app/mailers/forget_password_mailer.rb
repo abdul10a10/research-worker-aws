a30f5a012c092d1db9bb9173078a951bda64f350
Ashwin Choudhary <ashwin.choudhary@karyonsolutions.com>
2019-10-21 11:08:36 +0530

    send confirmation code with url in forget passworrd mail

--- app/mailers/forget_password_mailer.rb
+++ app/mailers/forget_password_mailer.rb
@@ -3,7 +3,7 @@ class ForgetPasswordMailer < ApplicationMailer
 
   def forget_password_email
     @user = params[:user]
-    @link = "http://winpowerllc.karyonsolutions.com/#/pages/updatepassword"
+    @link = "http://winpowerllc.karyonsolutions.com/#/pages/updatepassword/#{@user.reset_password_token}"
     mail(to: @user.email, subject: "Forget password link")
   end
 
