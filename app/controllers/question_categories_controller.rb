d3d68a6330959200a5ab8574700f001b6335f96e
Ashwin Choudhary <ashwin.choudhary@karyonsolutions.com>
2019-10-23 17:25:49 +0530

    checked whether user is admin or not in multiple functions resolved error3

--- app/controllers/question_categories_controller.rb
+++ app/controllers/question_categories_controller.rb
@@ -6,7 +6,7 @@ class QuestionCategoriesController < ApplicationController
   # GET /question_categories
   # GET /question_categories.json
   def index
-    if @current_user == "Admin"
+    if @current_user.user_type == "Admin"
       @question_categories = QuestionCategory.where(deleted_at: nil).order(id: :asc)
       render json: {Data: {question_categories: @question_categories}, CanEdit: false, CanDelete: false, Status: :ok, message: "question-categories", Token: nil, Success: true}, status: :ok
     else
