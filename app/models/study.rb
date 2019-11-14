51ad895562a02e36e66a7c1b8ae4f26bbfbce55c
10a10khan <abdulrahman.khan@codoxysolutions.com>
2019-11-14 11:25:27 +0530

    resolved error of blacklisted user list as model assosiation was not there

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,7 +1,9 @@
 class Study < ApplicationRecord
   has_many :audiences
   has_many :eligible_candidates
-  has_many :transactions  
+  has_many :transactions
+  has_many :whitelist_users
+  has_many :blacklist_users
   belongs_to :user
   
   def deleted_at!
