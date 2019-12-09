139b4af26790afe2dc8f2d171dc5761131148aef
10a10khan <abdulrahman.khan@codoxysolutions.com>
2019-12-09 18:06:51 +0530

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-UaePost.create(city: 'Abu Dhabi', po_box_number: '3838')
-UaePost.create(city: 'Abu Dhabi', po_box_number: '111188')
-UaePost.create(city: 'Ajman', po_box_number: '6263')
-UaePost.create(city: 'Al Ain', po_box_number: '88888')
-UaePost.create(city: 'Dubai', po_box_number: '88878')
-UaePost.create(city: 'Dubai', po_box_number: '111311')
-UaePost.create(city: 'Fujairah', po_box_number: '3999')
-UaePost.create(city: 'Ras Al Khaimah', po_box_number: '30999')
-UaePost.create(city: 'Sharjah', po_box_number: '32223')
-UaePost.create(city: 'Umm Al Quwain', po_box_number: '660')
