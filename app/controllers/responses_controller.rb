e3aee9232c7bf6149a6ffa3ed37a2046480cd64a
10a10khan <abdulrahman.khan@codoxysolutions.com>
2019-12-09 11:57:08 +0530

    changed form controller name to description when question type 4

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -31,13 +31,17 @@ class ResponsesController < ApplicationController
         end
       end
     elsif @question.question_type_id == 4
-      @range_response = RangeResponse.new()
-      @range_response.user_id = response_params[:user_id]
-      @range_response.question_id = response_params[:question_id]
-      @range_response.description = response_params[:answer_id]
-      @range_response.save
-      @message = @range_response.errors
-
+      if params[:description] == nil
+        @message = "please-select-valid-answer"
+      else
+        @range_response = RangeResponse.new()
+        @range_response.user_id = response_params[:user_id]
+        @range_response.question_id = response_params[:question_id]
+        @range_response.description = params[:description]
+        @range_response.save
+        @message = "response-saved"
+  
+      end
     else
       if Answer.where(id: response_params[:answer_id],question_id: response_params[:question_id], deleted_at: nil).present?
         if @response.save
